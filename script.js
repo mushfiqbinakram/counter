1d882a458d29d043b47b07fc2c1bfa17bad63635
Mushfiq Bin Akram <mushfiqbinakram@gmail.com>
2026-02-03 23:44:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let count=0
+let countEl=document.getElementById("count-el")
+function increment(){
+    // console.log("clicked")
+    count=count+1
+    countEl.innerHTML=count
+    // console.log(count)
+}
+function decrement(){
+    count=count-1
+    if(count>=0){
+        countEl.innerHTML=count
+    }
+}
+function reset(){
+    count=0
+    countEl.innerHTML=count
+}
